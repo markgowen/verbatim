bbe363d9cf20a56cc1ff277d5da40ddd49c47c05
Mark Gowen <mark.gowen78@gmail.com>
2022-11-15 12:39:01 -0800

    Refactored JS, finalized css, completed project

--- src/index.js
+++ src/index.js
@@ -8,54 +8,71 @@ function init() {
         fetch(`${url}${searchTerm}`)
         .then(res => res.json())
         .then(data => renderResult(data[0]))
-        .catch(() => {
-            result.innerHTML = "Could not find word"
-        })
+        // .catch(() => {
+        //     result.innerHTML = "Could not find word"
+        // })
     })
 }
 
 function renderResult(element) {
-    const result = document.querySelector(".result");
-    const details = document.querySelector(".details");
+    
+    let result = document.querySelector(".result");
     const word = document.querySelector(".word");
-
+    const details = document.querySelector(".details");
+    const wordDef = document.querySelector(".word-def");
+    const wordExample = document.querySelector(".example");
     const sound = document.querySelector("#sound");
-    sound.setAttribute("src", `${element.phonetics[0].audio}`);
+    
+    word.innerHTML = ""
+    details.innerHTML = ""
+    wordDef.innerHTML = ""
+    wordExample.innerHTML = ""
 
     const soundIcon = document.createElement("button");
-    soundIcon.innerText = "Play"
+    const icon = document.createElement("i");
+
+    icon.className = ("fas fa-volume-up")
+    soundIcon.append(icon)
     
     const term = document.createElement("h3");
     term.innerHTML = element.word;
     
     const phonetic = document.createElement("p");
-    phonetic.innerHTML = element.phonetic;
-
+    phonetic.innerHTML = element.phonetic || element.phonetics.find((p) => {
+        return p.text
+    }).text;
+    
     const partOfSpeech = document.createElement("p");
     partOfSpeech.innerText = element.meanings[0].partOfSpeech;
-    
-    details.append(phonetic, partOfSpeech)
-    word.append(sound, soundIcon, term)
-    result.append(word, soundIcon, details)
-    
-    // setting all definitions for each word
-    element.meanings.forEach((meaning) => {
-        
-        // const synonyms = document.createElement("p");
-        // synonyms.classList.add("details")
 
-        // synonyms.innerText = meaning.synonyms;
-
-        meaning.definitions.forEach((definitions) => {
-            const definition = document.createElement("p");
-            
-            definition.classList.add("definition");
-            definition.innerText = definitions.definition;
-            
-            result.append(definition)
+    const example = document.createElement("p");
+    element.meanings.find((m) => {
+        return m.definitions.find((d) => {
+            if (d.example){
+                example.innerText = d.example
+            }
+            return d.example
         })
     })
+
+    const definition = document.createElement("p");
+    definition.innerText = element.meanings[0].definitions[0].definition;
     
+    wordDef.append(definition)
+    wordExample.append(example)
+    details.append(phonetic, partOfSpeech)
+    word.append(term, soundIcon)
+    result.append(word, details, wordDef, wordExample)
+
+    const audio = element.phonetics.find((audioSrc) => {
+        return audioSrc.audio.length > 0})?.audio
+
+    sound.setAttribute("src", `${audio}`);
+    
+    if (!audio) {
+        soundIcon.remove();
+    }
+
     soundIcon.addEventListener("click", () => {
         playSound()
     })
@@ -65,4 +82,14 @@ function playSound() {
     sound.play();
 }
 
+function darkMode() {
+    const darkModeIcon = document.querySelector(".dark-mode-icon");
+
+    darkModeIcon.addEventListener("click", () => {
+        const element = document.body;
+        element.classList.toggle("dark-mode")
+    })
+}
+
+darkMode()
 init()
